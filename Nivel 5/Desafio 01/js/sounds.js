c40833f76355235f7d38e6005e0527b0b988a65c
Hersonmei <herson.me@hotmail.com>
2022-09-04 11:23:05 -0300

    Correção nos sons

--- Nivel 5/Desafio 01/js/sounds.js
+++ Nivel 5/Desafio 01/js/sounds.js
@@ -1,8 +1,8 @@
 export function Sounds(){
-    const audioForest = new Audio("https://github.com/Hersonmei/Explorer/blob/main/N%C3%ADvel%205/Desafio01/audio/Floresta.wav?raw=true")
-    const audioRain = new Audio("https://github.com/Hersonmei/Explorer/blob/main/N%C3%ADvel%205/Desafio01/audio/Chuva.wav?raw=true")
-    const audioCafeteria = new Audio("https://github.com/Hersonmei/Explorer/blob/main/N%C3%ADvel%205/Desafio01/audio/Cafeteria.wav?raw=true")
-    const audioFireplace = new Audio("https://github.com/Hersonmei/Explorer/blob/main/N%C3%ADvel%205/Desafio01/audio/Lareira.wav?raw=true")
+    const audioForest = new Audio("https://github.com/Hersonmei/Explorer/blob/main/Nivel%205/Desafio%2001/audio/Floresta.wav?raw=true")
+    const audioRain = new Audio("https://github.com/Hersonmei/Explorer/blob/main/Nivel%205/Desafio%2001/audio/Chuva.wav?raw=true")
+    const audioCafeteria = new Audio("https://github.com/Hersonmei/Explorer/blob/main/Nivel%205/Desafio%2001/audio/Cafeteria.wav?raw=true")
+    const audioFireplace = new Audio("https://github.com/Hersonmei/Explorer/blob/main/Nivel%205/Desafio%2001/audio/Lareira.wav?raw=true")
 
     
     function cancelAudio(){
