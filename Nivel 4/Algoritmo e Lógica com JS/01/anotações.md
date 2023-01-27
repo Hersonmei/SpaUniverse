efce3240d7b2b413ee1181f8d977598a1e319aa6
Hersonmei <herson.me@hotmail.com>
2023-01-27 11:54:52 -0300

    Atualização Repositório

--- Nivel 4/Algoritmo e Lógica com JS/01/anotações.md
+++
@@ -1,2 +0,0 @@
-1. Identifique o que já sabe e faça!
-2. Quebre o grande problema em problemas menores!
--- Nivel 4/Algoritmo e Lógica com JS/02/script.js
+++
@@ -1,6 +0,0 @@
-let numberOne = prompt("Digite o primeiro número: ")
-let numberTwo = prompt("Digite o segundo número: ")
-
-let sum = Number(numberOne) + Number(numberTwo)
-
-alert("O resultado é " + sum)
